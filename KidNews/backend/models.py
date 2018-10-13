a044f7af363d62ec8682b2a309019855187f63ec
osandvold302 <ollie302@gmail.com>
2018-10-13 17:38:07 -0500

    Created new article db object

--- KidNews/backend/models.py
+++ KidNews/backend/models.py
@@ -5,4 +5,10 @@ class user_reader(models.Model):
   name = models.CharField(max_length=100)
   age = models.PositiveIntegerField()
   gender = models.CharField(max_length=10)
-  
+
+class article(models.Model):
+  title = models.CharField(max_length=150)
+  source_link = models.URLField()
+  description = models.CharField(max_length=300)
+
+
