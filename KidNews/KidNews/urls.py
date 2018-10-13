e4473d183eda5c811f5330aca5d363967abdacfc
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2018-10-13 16:37:13 -0500

    Redirect to login

--- KidNews/KidNews/urls.py
+++ KidNews/KidNews/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Within frontend app, urls.py should redirect nothing to the splash page 
-    path('',include('frontend.urls')),
-    path('/accounts',include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='accounts/login/')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     
 ]
