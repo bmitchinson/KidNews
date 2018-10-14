1cb9ab7e25f903afffbe1e0b9cd3d278f0fbe830
osandvold302 <ollie302@gmail.com>
2018-10-13 21:00:28 -0500

    Return name of object

--- KidNews/backend/models.py
+++ KidNews/backend/models.py
@@ -8,15 +8,21 @@ class user_reader(models.Model):
   
   class Meta:
     ordering = ('name',)
+  
+  def __str__(self):
+    return self.name
 
 
 class article(models.Model):
   title = models.CharField(max_length=150)
   source_link = models.URLField()
   description = models.CharField(max_length=300)
-  tags = models.CharField(max_length=150)
+  tags = models.TextField()
   # connect many users to articles & vise versa
   users = models.ManyToManyField(user_reader)
 
   class Meta:
     ordering = ('title',)
+  
+  def __str__(self):
+    return self.title
