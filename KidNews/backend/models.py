9bef1991c0b7857e4405445cd756afdf41a9ded2
osandvold302 <ollie302@gmail.com>
2018-10-13 19:44:58 -0500

    Updated many to many relation

--- KidNews/backend/models.py
+++ KidNews/backend/models.py
@@ -5,10 +5,18 @@ class user_reader(models.Model):
   name = models.CharField(max_length=100)
   age = models.PositiveIntegerField()
   gender = models.CharField(max_length=10)
+  
+  class Meta:
+    ordering = ('name',)
+
 
 class article(models.Model):
   title = models.CharField(max_length=150)
   source_link = models.URLField()
   description = models.CharField(max_length=300)
   tags = models.CharField(max_length=150)
+  # connect many users to articles & vise versa
+  users = models.ManyToManyField(user_reader)
 
+  class Meta:
+    ordering = ('title',)
