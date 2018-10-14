53b670fcbd3e669420f899d466b87a6b270e9da8
benjamin-weinberg <BenWeinberg01@gmail.com>
2018-10-13 23:17:42 -0500

    add user_reader

--- KidNews/backend/api.py
+++ KidNews/backend/api.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets, permissions
 
-from .models import article
-from .serializers import ArticleSerializer
+from .models import article, user_reader
+from .serializers import ArticleSerializer,User_readerSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = article.objects.all()
     permission_classes = [permissions.AllowAny, ]
     serializer_class = ArticleSerializer
+
+class User_readerViewSet(viewsets.ModelViewSet):
+    queryset = user_reader.objects.all()
+    permission_classes = [permissions.AllowAny, ]
+    serializer_class = User_readerSerializer
--- KidNews/backend/serializers.py
+++ KidNews/backend/serializers.py
@@ -7,3 +7,8 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = article
         fields = ('id', 'title', 'source_link', 'description', 'tags',  'users',  )
+
+class User_readerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_reader
+        fields = ('id', 'name', 'age', 'gender',  )
